02a346440b23c4c04e7cf58961e680e806fd1edc
Everett Williams <everettwilliams@localhost.localdomain>
2020-12-31 13:46:45 -0500

    Refactored settings and password files

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+#psgt)+8@q4=0d1@s5#mnxacoy+9qf4h035v2h5y7v*@81b%&'
+SECRET_KEY = '+#psgt)+8@q4=aea;t&&0d1@s5#mnxacoy+9qf4h035v2h5y7v*@81b%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
